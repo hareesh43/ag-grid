69f9613ee5123302b37bc2f894faf391b27a4c2d
hareesh43 <hareeshgummani@gmail.com>
2021-03-25 19:50:20 +0530

    ag grid demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { AgGridColumn, AgGridReact } from "ag-grid-react";
 
-function App() {
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+
+const App = () => {
+  const [gridApi, setGridApi] = useState(null);
+  const [gridColumnApi, setGridColumnApi] = useState(null);
+  const [rowData, setRowData] = useState([]);
+
+  const [colData, setColData] = useState([
+    { headerName: "Make", field: "make", sortable: true, filter: true,checkboxSelection:true },
+    { headerName: "Model", field: "model" },
+    { headerName: "Price", field: "price" },
+  ]);
+
+  useEffect(() => {
+    fetch("https://www.ag-grid.com/example-assets/row-data.json")
+      .then((result) => result.json())
+      .then((rowData) => setRowData(rowData));
+  }, []);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const selectedNodes = gridApi.getSelectedNodes();
+    const selectedData = selectedNodes.map((node) =>node.data)
+    const selectedDataStringPresentation = selectedData.map((node) => node.make+' '+node.model+''+node.price)
+alert(`selected nodes ${selectedDataStringPresentation}`)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+      <button onClick={(e) => handleClick(e)}>Select</button>
+      <AgGridReact
+        rowData={rowData}
+        columnDefs={colData}
+        rowSelection="multiple"
+        onGridReady = {(params)=>setGridApi(params.api)}
+      />
     </div>
   );
-}
+};
 
 export default App;
